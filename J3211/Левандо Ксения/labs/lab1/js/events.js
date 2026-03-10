c38e8ffbe13e5ed2ded74ec29799a2d93f85e918
xlevando <xlevando@gmail.com>
2026-03-10 14:43:00 +0300

    feat: add event cards to events page

--- J3211/Левандо Ксения/labs/lab1/js/events.js
+++ J3211/Левандо Ксения/labs/lab1/js/events.js
@@ -1,3 +1,53 @@
+const events = [
+
+{
+    name: "Concert",
+    date: "21 Nov 2026",
+    city: "Paris",
+    image: ""
+},
+
+{
+    name: "Festival",
+    date: "5 Dec 2026",
+    city: "Berlin",
+    image: ""
+},
+
+{
+    name: "Theatre",
+    date: "14 Dec 2026",
+    city: "Milan",
+    image: ""
+}
+
+];
+
+function renderEvents() {
+        const container = document.getElementById("eventsContainer");
+        container.innerHTML = "";
+        events.forEach((event) => {
+            container.innerHTML += `
+            <div class="col-md-4">
+            <div class="card h-100 shadow-sm">
+            <img src="${event.image}" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text text-muted">
+            ${event.city} · ${event.date}
+            </p>
+            <button class="btn btn-warning"
+            onclick="buyTicket('${event.name}','${event.date}')">
+            Buy ticket
+            </button>
+            </div>
+            </div>
+            </div>
+            `;
+        });
+}
+
+
 function buyTicket(eventName, eventDate) {
     if (localStorage.getItem("auth") !== "true") {
     alert("Please login to buy tickets");
@@ -18,3 +68,5 @@ function buyTicket(eventName, eventDate) {
     alert("Ticket purchased!");
 
 }
+
+renderEvents();
