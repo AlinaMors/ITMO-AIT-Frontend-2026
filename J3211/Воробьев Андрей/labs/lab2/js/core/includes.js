b4b0965b442b15a411491e38381963ae75637ac2
vorandpav <vorandpav@yandex.ru>
2026-03-30 00:15:05 +0300

    feat: postlogin-navbar

--- J3211/Воробьев Андрей/labs/lab2/js/core/includes.js
+++ J3211/Воробьев Андрей/labs/lab2/js/core/includes.js
@@ -10,9 +10,26 @@ async function includeHTML(selector, url) {
     }
 }
 
+function setupPostLoginNavbar() {
+    const user = JSON.parse(localStorage.user);
+    const emailSpan = document.querySelector("#userEmail");
+    if (emailSpan) emailSpan.textContent = user.email;
+
+    const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
+
+    document.querySelectorAll("[data-page]").forEach(link => {
+        link.classList.toggle("active", link.getAttribute("data-page") === currentPage);
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.accessToken) { // Убрал переменную isLoggedIn
+        includeHTML("#postlogin-site-navbar", "fragments/postlogin_navbar.html").then(() => {
+            setupPostLoginNavbar();
+        });
+    } else {
+        includeHTML("#prelogin-site-navbar", "fragments/prelogin_navbar.html");
+    }
 
-    includeHTML("#prelogin-site-navbar", "fragments/prelogin_navbar.html");
     includeHTML("#site-footer", "fragments/footer.html");
-
 });
